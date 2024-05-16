223408f6d1467f827c6c64801e684d49d02b9006
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-16 13:03:55 +1000

    connects app to the database

--- server/app.js
+++ server/app.js
@@ -4,13 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors= require('cors')
-
+const mongoose = require ('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const blogRouter = require ('./routes/blog')
+const blogRouter = require ('./routes/blog');
+require('dotenv').config();
+
+///-----CONNECTION TO DB-----///
+
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI || process.env.DB_STRING_DEV;
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log('Connected to Database')
+}
+///---------------------------///
+
+
 
 var app = express();
 app.use(cors())
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
