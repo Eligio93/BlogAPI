04d3e36b37d36228ab04c17d6c6c7ee22e637a34
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-16 12:15:24 +1000

    adds env variable in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const fetchUrl=import.meta.env.VITE_DEV_BASEURL || import.meta.env.VITE_PROD_URL
+
 function App() {
   const [message, setMessage] = useState()
   useEffect(()=>{
-    fetch('http://localhost:3000/blog')
+    fetch(fetchUrl)
     .then(res=>res.json())
     .then(data=>setMessage(data.message))
   })
