ea607c3328c9aa67df06d70fba224cb1aaa25ca8
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-20 11:23:13 +1000

    delete and reset preloaded files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,14 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 const fetchUrl=import.meta.env.VITE_DEV_BASEURL || import.meta.env.VITE_PROD_URL
 
 function App() {
-  const [message, setMessage] = useState()
-  useEffect(()=>{
-    fetch(fetchUrl)
-    .then(res=>res.json())
-    .then(data=>setMessage(data.message))
-  })
-
   return (
     <>
-      <h1>{message}</h1>
+      <h1>Ciao</h1>
     </>
   )
 }
