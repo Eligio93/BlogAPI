8f32369f6bf8a774ef775c8c70e60b5c6930144c
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-23 10:43:49 +1000

    creates router with layout and children routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './routes/App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Signup from './routes/Signup.jsx'
+import Login from './routes/Login.jsx'
+import Layout from './Layout.jsx'
+import { AuthProvider } from '../components/AuthContext.jsx'
+import './style.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/signup',
+        element: <Signup />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  },
+
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
