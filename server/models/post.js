b784d2d80b009907b5f8775bb4a351474dc8bd6e
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-28 09:56:45 +1000

    fixes bug in Post Schema related to comments

--- server/models/post.js
+++ server/models/post.js
@@ -5,11 +5,13 @@ const PostSchema= new Schema({
     title:{type:String, required:true},
     date:{type:String, required:true},
     body_text:{type:String,required:true},
-    comments:[{type:Schema.Types.ObjectId, ref:'Comment', required:true}],
-    author:{type:Schema.Types.ObjectId, ref:'User', required:true},
+    comments:[{type:Schema.Types.ObjectId, ref:'Comment'}],
+    author:/*{type:Schema.Types.ObjectId, ref:'User'}*/{type:String, required:true},
     published:{type:Boolean, required:true}
 })
 
 PostSchema.virtual('url').get(function(){
     return `/blog/posts/${this._id}`
 })
+
+module.exports = mongoose.model('Post', PostSchema)
