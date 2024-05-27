ffe03057843c255c81ba9d1dd04254c202737b27
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-28 09:55:58 +1000

    fixed export in Comment Model

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    message:{type:String, required:true},
-    date:{type:String, required:true},
-    author:{type:Schema.Types.ObjectId, ref:'User', required:true}
+    message: { type: String, required: true },
+    date: { type: String, required: true },
+    author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 })
+
+module.exports = mongoose.model('Comment', CommentSchema)
