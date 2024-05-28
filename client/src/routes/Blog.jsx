a27d54f4fb6ec5648e9edea92dee0382846a4fd9
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-28 23:27:21 +1000

    defines first structure of Blog Page

---
+++ client/src/routes/Blog.jsx
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import postImg from '../img/postImg.jpg'
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../components/AuthContext";
+
+export default function Blog() {
+    const [posts, setPosts] = useState()
+    const [featuredPosts, setFeaturedPosts] = useState()
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState();
+    const { user } = useContext(AuthContext)
+
+    //insert error variable and loading icon in the finally
+    useEffect(() => {
+        async function getPosts() {
+            try {
+                let results = await axios.get('http://localhost:3000/blog/posts')
+                setPosts(results.data)
+                setFeaturedPosts(results.data.filter((post) => post.featured))
+            } catch (err) {
+                setError(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+        getPosts()
+    }, [])
+
+
+    // const featuredPosts= posts.filter((post)=>post.featured)
+    if (loading) {
+        return <p>Loading...</p>
+    }
+    if (error) {
+        return <p>{error}</p>
+    }
+
+    return (
+        <div className="blog-section">
+            <aside className="blog-aside">
+                {user && <div className="blog-user">
+                    <h2>Welcome {user.name}</h2>
+                    <Link to='/posts/newPost'>Create new Post</Link>
+                </div>}
+
+                <div className="featured-posts">
+                    <h2>Featured Posts</h2>
+                    <ul>
+                        {featuredPosts ? (
+                            featuredPosts.map((post) =>
+                                <li key={post._id}>
+                                    <p>{post.title}</p>
+                                </li>
+                            )
+                        ) : (<p>There s no featured posts</p>)}
+                    </ul>
+                </div>
+            </aside>
+          
+                {posts ? (
+                    <ul className="post-list">
+                        {posts.map((post) =>
+
+                            <li className="post-preview" key={post._id}>
+                                <img src={post.img} alt="" />
+                                <div className="post-preview-info">
+                                    <h2>{post.title}</h2>
+                                    <p>{post.description}</p>
+                                    <Link to={`/posts/${post._id}`} className="read-more-btn">Read More</Link>
+                                </div>
+
+                            </li>
+
+                        )}
+                    </ul>
+                ) : (
+                    <p>There s no post to be seen </p >
+                )
+                }
+          
+
+        </div>
+    )
+}
