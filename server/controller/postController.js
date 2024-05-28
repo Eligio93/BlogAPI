c1fae36dda0a8b7d82be9ce32ee9e3ae6f1a337e
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-28 23:28:26 +1000

    defines GET post's routes

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -1,26 +1,35 @@
 const Post = require('../models/post')
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose')
 
 /*GET all posts*/
-exports.posts=asyncHandler(async (req,res,next)=>{
-    let posts = await Post.find({published:true});
-   res.json(posts)
+exports.posts = asyncHandler(async (req, res, next) => {
+    let posts = await Post.find({ published: true });
+    res.json(posts)
 })
 
-
-
-
+/*Get Specific post*/
+exports.post_get = asyncHandler(async (req, res, next) => {
+    let postId = req.params.postId
+    let post = await Post.findById(postId).populate({
+        path: 'comments',
+        populate: {
+            path: 'author'
+        }
+    })
+    res.json(post)
+})
 
 /*POST new post*/
-exports.newPost_post=asyncHandler(async (req,res,next)=>{
-    const post= new Post({
-        title:req.body.title,
-        date:req.body.date,
-        body_text:req.body.body_text,
-        comments:req.body.comments,
-        author:req.body.author,
-        published:req.body.published
+exports.newPost_post = asyncHandler(async (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        date: req.body.date,
+        body_text: req.body.body_text,
+        comments: req.body.comments,
+        author: req.body.author,
+        published: req.body.published
     })
     await post.save()
-    res.json({message:'Post Created'})
+    res.json({ message: 'Post Created' })
 })
