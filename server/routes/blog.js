3a045e39f0a6796e0ff6c9e582fc15e7c9e5defb
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-30 13:36:04 +1000

    adds multer middleware to manage the upload

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const postController = require('../controller/postController')
+const commentController = require ('../controller/commentController')
 const userController = require('../controller/userController');
 const passport = require('../passport-config');
+const multer = require('multer')
+// const storage= multer.memoryStorage()
+const upload = multer({dest: 'uploads/'})
 
 /*Get All Posts*/
 router.get('/posts',postController.posts)
 
+/*GET Specific post*/
+router.get('/posts/:postId',postController.post_get);
 
+/*POST new Post *PROTECTED* */
+router.post('/posts/newPost',upload.single('img'),postController.newPost_post)
 
-
+/*POST comment*/
+router.post('/posts/:postId/comments/newComment',commentController.comment_post)
 
 
 
