643386d1daabbb8b882831bef8b42bda9f212a4d
Eligio93 <eligio.cristantielli@gmail.com>
2024-05-31 15:56:22 +1000

    structure and formats code

--- client/components/Comment.jsx
+++ client/components/Comment.jsx
@@ -3,10 +3,13 @@ import avatarImg from '../src/img/userAvatar.svg'
 export default function Comment({ message, date, author }) {
     return (
         <>
-            <div className="comment-author">
+            <div className="comment-info">
                 <img src={avatarImg} alt="comment's author avatar" />
-                <p>{author.name}</p>
-                <p>{date}</p>
+                <div className='comment-author'>
+                    <p>{author.name}</p>
+                    <p className='comment-date'>{date}</p>
+                </div>
+
             </div>
             <p className='comment-message'>{message}</p>
         </>
