99ebe5f315b4613465fc57a5375696ff32989bc6
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-04 21:56:21 +1000

    updates post Schema

--- server/models/post.js
+++ server/models/post.js
@@ -4,9 +4,10 @@ const Schema= mongoose.Schema;
 const PostSchema= new Schema({
     title:{type:String, required:true},
     date:{type:String, required:true},
+    description:{type:String, required:true},
     body_text:{type:String,required:true},
     comments:[{type:Schema.Types.ObjectId, ref:'Comment'}],
-    author:/*{type:Schema.Types.ObjectId, ref:'User'}*/{type:String, required:true},
+    author:/*{type:Schema.Types.ObjectId, ref:'User'},*/ {type:String, required:true},
     published:{type:Boolean, required:true},
     featured:{type:Boolean, required:true},
     img:{type:String, required:true}
