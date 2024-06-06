a7ac98edcff5895c09d8cd428ea867d42d17d0a6
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-06 11:24:55 +1000

    formats comment date

--- client/components/Comment.jsx
+++ client/components/Comment.jsx
@@ -1,13 +1,16 @@
 import avatarImg from '../src/img/userAvatar.svg'
+import { format } from 'date-fns'
 
 export default function Comment({ message, date, author }) {
+    let formatDate = format(date, "dd/MM/yyyy H':'m ")
+
     return (
         <>
             <div className="comment-info">
                 <img src={avatarImg} alt="comment's author avatar" />
                 <div className='comment-author'>
                     <p>{author.name}</p>
-                    <p className='comment-date'>{date}</p>
+                    <p className='comment-date'>{formatDate}</p>
                 </div>
 
             </div>
