3a734eeb014ba5076c363093a4ac5bbf415c62bb
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-11 15:12:20 +1000

    initialize the router

--- CMS/src/main.jsx
+++ CMS/src/main.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Layout from './Layout.jsx'
+import Login from './routes/Login.jsx'
 import NewPost from './routes/NewPost.jsx'
 import { AuthProvider } from '../components/AuthContext.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/newPost",
-    element: <NewPost />
-  },
+    path: '/',
+    element: <Layout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: "/newPost",
+        element: <NewPost />
+      }
+
+    ]
+  },{
+    path:'/login',
+    element:<Login />
+  }
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
