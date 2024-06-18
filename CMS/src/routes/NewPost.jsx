8a8141743b7bd5f2b85ff1e8be9c00ee02bdfee6
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-18 20:10:38 +1000

    import form component

--- CMS/src/routes/NewPost.jsx
+++ CMS/src/routes/NewPost.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react';
 import axios from 'axios'
 import { useContext } from 'react';
 import { AuthContext } from '../../components/AuthContext';
-import { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
 import { useNavigate } from 'react-router-dom';
+import Form from '../../components/Form';
 
 
 
 export default function NewPost() {
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
-    const editorRef = useRef(null);
     const [success, setSuccess] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(false)
@@ -71,6 +69,13 @@ export default function NewPost() {
             ...prevData, [name]: value
         }))
     }
+    //handle Post Editor Change
+    const handleEditor = (newValue, editorRef) => {
+        setData((prevData) => ({
+            ...prevData, body: editorRef.getContent({ format: 'text' })
+        }))
+    }
+
 
     if (loading) {
         return <p>Loading...</p>
@@ -78,54 +83,18 @@ export default function NewPost() {
     return (
         <>
             {success ? (<p>{success}</p>) : (
-                <form encType='multipart/form-data' className='form' onSubmit={handleSubmit}>
-                    {error && <p>{error}</p>}
-                    <label htmlFor="post-title"> Post Title:
-                        <input type="text" name='title' id='post-title' value={data.title} onChange={handleTextInput} />
-                    </label>
-                    <label htmlFor="post-description">Post Description:
-                        <textarea type="text" name='description' id='post-description' value={data.description} onChange={handleTextInput} />
-                    </label>
-                    <label htmlFor="post-body">Post Body:
-                        <Editor
-                            name='body'
-                            id='post-body'
-                            apiKey={import.meta.env.VITE_TINY_MCE_API}
-                            value={data.body}
-                            onInit={(_evt, editor) => editorRef.current = editor}
-                            // initialValue={data.body}
-                            onEditorChange={(newValue, editorRef) => {
-                                setData((prevData) => ({
-                                    ...prevData, body: editorRef.getContent({ format: 'text' })
-                                }))
-                            }}
-                            init={{
-                                height: 300,
-                                menubar: false,
-                                plugins: [
-                                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-                                    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-                                    'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-                                ],
-                                toolbar: 'undo redo |' +
-                                    'bold italic forecolor | alignleft aligncenter ' +
-                                    'alignright alignjustify | bullist numlist outdent indent | ' +
-                                    'removeformat',
-                                content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-                            }}
-                        />
-                    </label>
-                    <label htmlFor="img">Upload post Image:
-                        <input id='img' type="file" name='img' onChange={handleFile} />
-                    </label>
-                    <label htmlFor="post-published">Published?
-                        <input type="checkbox" name="published" id="post-published" checked={data.published} onChange={handleCheckbox} />
-                    </label>
-                    <label htmlFor="post-featured">Featured?
-                        <input type="checkbox" name="featured" id="post-featured" checked={data.featured} onChange={handleCheckbox} />
-                    </label>
-                    <button type='submit'>Submit</button>
-                </form>
+                <>
+                {error && <p>{error}</p>}
+                <Form
+                    handleSubmit={handleSubmit}
+                    handleTextInput={handleTextInput}
+                    handleFile={handleFile}
+                    handleCheckbox={handleCheckbox}
+                    handleEditor={handleEditor}
+                    data={data}
+                    disableField={false}
+                />
+                </>
             )}
 
         </>
