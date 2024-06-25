092b8c291c4960f66407fca3a311d39301d340d6
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-25 23:53:29 +1000

    adds link to logout

--- CMS/src/Layout.jsx
+++ CMS/src/Layout.jsx
@@ -1,25 +1,38 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../components/AuthContext";
 
-export default function Layout(){
+export default function Layout() {
+    const { user, logOut } = useContext(AuthContext)
 
-
-    return(
+    return (
         <>
-        <div className="sidebar">
-            <h2>Sidebar</h2>
-            <ul>
-                <li>
-                    <Link to='/posts'>All Posts</Link>
-                </li>
-                <li>
-                    
-                </li>
-            </ul>
-        </div>
-        <div className="content">
-            <h2>Content</h2>
-        </div>
+            <div className="sidebar">
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/posts'>All Posts</Link>
+                    </li>
+                    <li>
+                        <Link to='/posts/status/published'>Published Posts</Link>
+                    </li>
+                    <li>
+                        <Link to='/posts/status/unpublished'>Unpublished posts</Link>
+                    </li>
+                    <li>
+                        <Link to='/newPost'>Add New Post</Link>
+                    </li>
+                    {user && <li>
+                        <Link onClick={logOut} >LogOut</Link>
+                    </li>}
+                </ul>
+            </div>
+            <div className="content">
+                <Outlet />
+            </div>
         </>
     )
 }
