01220feb247dea1b12e35e8407b47272030b5e1f
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-25 23:54:51 +1000

    send user to login in case is not logged

--- CMS/src/routes/Home.jsx
+++ CMS/src/routes/Home.jsx
@@ -1,3 +1,15 @@
-export default function Home(){
-    return <h1>Home</h1>
+import { useContext, useEffect, useState } from "react"
+import axios from "axios"
+import { AuthContext } from "../../components/AuthContext"
+import { useNavigate } from "react-router-dom"
+export default function Home() {
+ const {user}= useContext(AuthContext)
+ const navigate = useNavigate()
+useEffect(()=>{
+    if(!user){
+        navigate('/login',{state:{message:'You need to be logged in to see the content'}})
+     }
+
+})
+
 }
