7980e19cf40a50e61ba1e923b17857c51ca1cc77
Eligio93 <eligio.cristantielli@gmail.com>
2024-06-26 15:12:14 +1000

    adds minLength to password

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const UserSchema= new Schema({
     name:{type:String, required:true},
     lastName:{type:String, required:true},
     email:{type:String, required:true},
-    password:{type:String, required:true},
+    password:{type:String, required:true, minLength: 6},
     admin:{type:Boolean}
 })
 
