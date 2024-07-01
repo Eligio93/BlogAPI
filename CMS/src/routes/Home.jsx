978685a5985a422458ffa063efd67783ffc53647
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-01 19:41:43 +1000

    init homepage

--- CMS/src/routes/Home.jsx
+++ CMS/src/routes/Home.jsx
@@ -1,15 +1,21 @@
 import { useContext, useEffect, useState } from "react"
-import axios from "axios"
 import { AuthContext } from "../../components/AuthContext"
 import { useNavigate } from "react-router-dom"
 export default function Home() {
-    const { user } = useContext(AuthContext)
+    const { user, jwt} = useContext(AuthContext)
     const navigate = useNavigate()
     useEffect(() => {
         if (!user) {
             navigate('/login', { state: { message: 'You need to be logged in to see the content' } })
         }
 
-    })
+    },[])
+
+
+    return(
+        <>
+        {user && <h2>Welcome {user.name}</h2>}
+        </>
+    )
 
 }
