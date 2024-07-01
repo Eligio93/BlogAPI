7b1b99f58c8e87db09477e54c6dbde0f34fd38a8
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-01 23:37:38 +1000

    changes date data type

--- server/models/post.js
+++ server/models/post.js
@@ -3,7 +3,7 @@ const Schema= mongoose.Schema;
 
 const PostSchema= new Schema({
     title:{type:String, required:true},
-    date:{type:String, required:true},
+    date:{type:Date, required:true},
     description:{type:String, required:true},
     body_text:{type:String,required:true},
     comments:[{type:Schema.Types.ObjectId, ref:'Comment'}],
