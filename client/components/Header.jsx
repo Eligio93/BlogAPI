1fa81a2686153745b19b08659465ac0a08ce20ae
Eligio93 <eligio.cristantielli@gmail.com>
2024-07-02 17:39:26 +1000

    creates mobile menu

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,22 +1,44 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import loginIcon from "../src/img/loginIcon.svg"
 import registerIcon from "../src/img/registerIcon.svg"
-import logoutIcon   from "../src/img/logoutIcon.svg"
+import logoutIcon from "../src/img/logoutIcon.svg"
+import hamburgerIcon from "../src/img/hamburgerIcon.svg"
 
 
 
 function Header() {
     const { user, setUser, logOut } = useContext(AuthContext);
-
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const location = useLocation()
+    useEffect(()=>{
+        setMobileMenu(false)
+    },[location])
 
     return (
         <header>
+            <div className="mobile-menu">
+                <img onClick={() => setMobileMenu(!mobileMenu)} className="menuIcon" src={hamburgerIcon} alt="" />
+                <div className='mobile-nav' hidden={!mobileMenu} style={{ display: mobileMenu ? 'flex' : 'none' }}>
+                    {user ? (<div className="auth-btn">
+                        <img className='authIcon' src={logoutIcon} alt="" />
+                        <Link onClick={logOut}>Logout</Link>
+                    </div>) : (<><div className="auth-btn">
+                        <img className='authIcon' src={loginIcon} alt="" />
+                        <Link to='/login'>Login</Link>
+                    </div>
+                        <div className="auth-btn">
+                            <img className='authIcon' src={registerIcon} alt="" />
+                            <Link to='/signup'>Register</Link>
+                        </div></>)}
+
+                </div>
+            </div>
             <div className="auth-area">
                 {user ? (
                     <div className="auth-btn">
-                        <img className='authIcon'src={logoutIcon} alt="" />
+                        <img className='authIcon' src={logoutIcon} alt="" />
                         <Link onClick={logOut}>Logout</Link>
                     </div>
                 ) : (
